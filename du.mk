d9c407ad281fc0b117d51afcb138cb821fff02bd
Jake Whatley <jacob.whatley91@gmail.com>
2014-10-12 16:52:39 -0400

    DU support
    Change-Id: I41296c6f5f0d0307fe559c3e5c15783e657e51e4

--- du.mk
+++ du.mk
@@ -13,19 +13,19 @@
 # limitations under the License.
 
 # Inherit APNs list
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/du/config/common.mk)
 
 $(call inherit-product-if-exists, vendor/lge/galbi/galbi-gsm-vendor.mk)
 $(call inherit-product, device/lge/g2-common/g2.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_f320
+PRODUCT_NAME := du_f320
 PRODUCT_DEVICE := f320
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := lge
